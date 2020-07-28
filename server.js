7eb9fac01424b8c34f3886d53d07c00437c4b218
Tosin Daniel <osdaniel024@gmail.com>
2020-07-29 01:17:26 +1000

    added a sum function

--- server.js
+++ server.js
@@ -8,3 +8,15 @@ app.use(express.static(__dirname +'/public'));
 let port = 3000;
 app.listen(port);
 console.log('Server listening on port '+port)
+
+let addition = (numA, numB)=> {
+    let sum = numA + numB;
+    return sum;
+}
+
+app.get('/addition',function(req,res){
+    let numA=parseInt(req.query.numA);
+    let numB=parseInt(req.query.numB);
+    let sum=addition(numA,numB);
+    res.send('Sum of numbers is:' +sum);
+})
